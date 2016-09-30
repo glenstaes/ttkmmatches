95b4f51f9220cb2ad8887d4dc67915eb1baa8dc4
Glen Staes <staeseke@gmail.com>
2016-09-30 16:47:58 +0200

    Submit loginform on enter

--- app/js/controllers/LoginController.js
+++ app/js/controllers/LoginController.js
@@ -7,30 +7,49 @@
         UserService.redirectIfLoggedIn();
 
         /**
-         * @function login 
+         * @function ctrl.login 
          * @description Tries to login the user with the UserService.
          */
         ctrl.login = function () {
-            UserService.login(ctrl.user.loginName, ctrl.user.password).then(function (response) {
-                if (UserService.isLoggedIn()) {
-                    // Create a toast that welcomes the user.
-                    var toast = $mdToast.simple().content(["Welkom ", response.user.firstName, " ", response.user.lastName, "!"].join("")).theme("success-toast").position("top right");
+            if (angular.isUndefined(ctrl.user)) {
+                // Create a toast that warns the user.
+                var toast = $mdToast.simple().content(["Fout: Geen logingegevens opgegeven"].join("")).theme("error-toast").position("top right");
 
-                    // Show the toast
-                    $mdToast.show(toast);
+                // Show the toast
+                $mdToast.show(toast);
+            } else
+                UserService.login(ctrl.user.loginName, ctrl.user.password).then(function (response) {
+                    if (UserService.isLoggedIn()) {
+                        // Create a toast that welcomes the user.
+                        var toast = $mdToast.simple().content(["Welkom ", response.user.firstName, " ", response.user.lastName, "!"].join("")).theme("success-toast").position("top right");
 
-                    UserService.redirectIfLoggedIn();
-                }
-            }).catch(function (response) {
-                if(angular.isDefined(response.error)){
-                    // Create a toast that welcomes the user.
-                    var toast = $mdToast.simple().content(["Fout: ", ErrorService.getLocalErrorMessage(response.error.code)].join("")).theme("error-toast").position("top right");
+                        // Show the toast
+                        $mdToast.show(toast);
 
-                    // Show the toast
-                    $mdToast.show(toast);
-                }
-            });
-        }
+                        UserService.redirectIfLoggedIn();
+                    }
+                }).catch(function (response) {
+                    if ((angular.isDefined(response.data) && angular.isDefined(response.data.error)) || angular.isDefined(response.error)) {
+                        // Create a toast that warns the user.
+                        var toast = $mdToast.simple().content(["Fout: ", ErrorService.getLocalErrorMessage(response.data ? response.data.error.code : response.error.code)].join("")).theme("error-toast").position("top right");
+
+                        // Show the toast
+                        $mdToast.show(toast);
+                    }
+                });
+        };
+
+        /**
+         * @function ctrl.onInputKeyUp
+         * @description Manages the key up event of the form inputs. Submits the form if the enter key is pressed.
+         */
+        ctrl.onInputKeypress = function (e) {
+            var code = e.which || e.keyCode;
+
+            if (code === 13) {
+                ctrl.login();
+            }
+        };
     };
 
     angular.module("matches").controller("LoginController", ["$mdToast", "UserService", "ErrorService", LoginController]);
