54b5dd5ca6de1645eb890887af434e0ada0b2457
Glen Staes <staeseke@gmail.com>
2016-10-14 17:45:25 +0200

    Gulp browser sync task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 // Load in the dependencies
 var gulp = require("gulp");
 var Server = require("karma").Server;
+var browserSync = require("browser-sync").create();
 
 // Run the test suite once
 gulp.task("testsingle", function(done){
@@ -14,4 +15,13 @@ gulp.task("test", function(done){
     new Server({
         configFile: __dirname + "/karma.conf.js", singleRun: false
     }, done).start();
+});
+
+// Run browsersync and watch changes on the files
+gulp.task("serve", function(){
+    browserSync.init({
+        proxy: "ttkmmatches"
+    });
+
+    gulp.watch(["app/*.*", "app/**/*.*"]).on("change", browserSync.reload);
 });
