2b23a4b2d6967da123023ab4e685ba1ae42ea7a9
Glen Staes <staeseke@gmail.com>
2016-10-16 17:15:37 +0200

    Provide documentation for the api endpoints

--- backend/app/Http/Controllers/SeasonController.php
+++ backend/app/Http/Controllers/SeasonController.php
@@ -11,6 +11,14 @@ use App\Season;
 
 class SeasonController extends Controller
 {
+    /**
+     * getSeasons
+     *
+     * Gets all the seasons from the database.
+     *
+     * @param (Request) An instance of the Request object. 
+     * @return (Array<Season>) An array of Season objects.
+     */
     public function getSeasons(Request $request){
         return Response::json(Season::all());
     }
--- backend/app/Http/Controllers/Tabt.php
+++ backend/app/Http/Controllers/Tabt.php
@@ -11,11 +11,23 @@ use SoapClient;
 
 class Tabt extends Controller
 {
+    /**
+     * __construct
+     *
+     * Sets the api urls as a constant.
+     */
     public function __construct(){
         define("TABT_API_VTTL", "http://api.vttl.be/0.7/?wsdl");
         define("TABT_API_SPORTA", "http://ttonline.sporta.be/api/?wsdl");
     }
 
+    /**
+     * connectToTabT
+     *
+     * Uses the api urls to instantiate an object with a SoapClient for the Api's.
+     *
+     * @return (object) An object containing two attributes with a SoapClient instance.
+     */
     private function connectToTabT(){
         $connections = app("stdClass");
         $connections->VTTL = new SoapClient(constant("TABT_API_VTTL"));
@@ -23,6 +35,15 @@ class Tabt extends Controller
         return $connections;
     }
 
+    /**
+     * getMembers
+     *
+     * Connects to TabT to retrieve the members of the club in the current season. Be aware 
+     * that this uses the current season as known in the TabT database and not our own database.
+     *
+     * @param (Request) An instance of the Request object. 
+     * @return (object) An object with the members for each federation.
+     */
     public function getMembers(Request $request){
         try{
             $connection = $this->connectToTabT();
@@ -45,15 +66,18 @@ class Tabt extends Controller
         }
     }
 
+    /**
+     * getSeasons
+     *
+     * Connects to TabT to retrieve the seasons.
+     * @param (Request) An instance of the Request object. 
+     * @return (object) An object containing the seasons count and an array with season entries.
+     */
     public function getSeasons(Request $request){
         try{
             $connection = $this->connectToTabT();
 
-            $response = app("stdClass");
-            $response->VTTL = $connection->VTTL->GetSeasons();
-            $response->Sporta = $connection->Sporta->GetSeasons();
-
-            return Response::json($response);
+            return Response::json($connection->VTTL->GetSeasons());
         } catch(\Exception $ex){
             return Response::json($ex->getMessage());
         }
