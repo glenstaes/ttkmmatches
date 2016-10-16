49cf11616df3b339b822d7b3b404ef98d48f64b7
Glen Staes <staeseke@gmail.com>
2016-10-16 17:25:40 +0200

    Split members list into separate component

---
+++ app/js/components/members-list/component.js
@@ -0,0 +1,14 @@
+(function(){
+
+    var membersListComponent = function(){
+        var ctrl = this;
+    };
+
+    angular.module("matches").component("membersList", {
+        templateUrl: "app/js/components/members-list/template.html",
+        controller: [membersListComponent],
+        bindings: {
+            members: "<"
+        }
+    });
+})();
